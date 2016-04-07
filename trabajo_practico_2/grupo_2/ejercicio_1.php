30b374d318677e307a9bc390a0531342a685485e
lauuteeh <lautaro.aragones@live.com.ar>
2016-04-07 01:41:05 -0300

    Se realizo el ejercicio 1 del grupo 2

--- trabajo_practico_2/grupo_2/ejercicio_1.php
+++ trabajo_practico_2/grupo_2/ejercicio_1.php
@@ -6,16 +6,28 @@
 <body>
 
 		<!--Ejercicio Nro. 1: Dada una variable en php realice un algoritmo que imprima un nuevo valor a partir de calcular el valor de la variable multiplicada por la iteración en que se encuentre el algoritmo. Repita el proceso hasta que el valor del resultado supere el valor de la variable elevado al valor de la variable.
-.
-.
 		-->	
 
 		<?php
 
+		$z=0;
+		$j=10;
+		$x=0;
+
+		while ( $z < ($j*$j)){
+				
+			$x=$x+1;
+
+			$z=$j*$x;
+			echo "$z <br>";
+
+			
+		} ;
 			
 
 
 		?>	
+		
 
 
 
